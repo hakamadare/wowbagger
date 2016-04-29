03ce7029373abedc99514656e57c65b601b680a6
Steve Huff <steve.huff@runkeeper.com>
2016-04-29 15:51:00 -0400

    argh

--- lib/wowbagger/aws.rb
+++ lib/wowbagger/aws.rb
@@ -41,27 +41,21 @@ module Wowbagger
 
         until next_marker.nil?
           selected = lbs.load_balancer_descriptions.select do |lbd|
-            begin
-              lb_name = lbd.load_balancer_name
+            lb_name = lbd.load_balancer_name
 
-              /^rk-prod-app/ =~ lb_name
+            lb_tags = self.elasticloadbalancing.describe_tags(load_balancer_names: [lb_name])
 
-              # lb_tags = self.elasticloadbalancing.describe_tags(load_balancer_names: [lb_name]).tag_descriptions[0].tags.collect {|tag| [tag.key, tag.value]}.flatten
-              #
-              # lb_tags_hash = Hash(*lb_tags)
-              #
-              # puts lb_tags_hash.inspect
-              #
-              # exit
-              #
-              # if all
-              #   lb_tags.tag_descriptions.all? {|tag| tags.include?(tag)}
-              # else
-              #   lb_tags.any? {|tag| tags.include?(tag)}
-              # end
+            tag_hashes = lb_tags.tag_descriptions.collect do |td|
+              extract_elb_tags_as_hash(td)
+            end
 
-            rescue ArgumentError => e
-              $stderr.puts "#{lb_name} has no tags"
+            tag_hashes.select do |tag_hash|
+              $stderr.puts tag_hash.inspect
+              if all
+                tag_hash.all? tags
+              else
+                tag_hash.any? tags.to_a
+              end
             end
           end
 
@@ -97,6 +91,31 @@ module Wowbagger
         address = IPAddr.new(address)
 
         address
+
+      rescue StandardError => e
+        raise e
+      end
+    end
+
+    def extract_elb_tags_as_hash(tag_description)
+      begin
+        unless tag_description.kind_of?(Aws::ElasticLoadBalancing::Types::TagDescription)
+          raise "'#{tag_description.inspect}' is not an Aws::ElasticLoadBalancing::Types::TagDescription" 
+        end
+
+        tags = tag_description.tags
+
+        keys = []
+        values = []
+
+        tags.each do |tag|
+          keys.push(tag.key)
+          values.push(tag.value)
+        end
+
+        zipped = keys.zip(values).flatten
+
+        Hash[*zipped]
 
       rescue StandardError => e
         raise e
